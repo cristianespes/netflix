7b20f150df969786ab1def9ad528b9d73a916c92
Cristian Espes <christian_zgza@hotmail.com>
2021-03-20 11:00:52 +0100

    First commit

---
+++ Shared/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Shared
+//
+//  Created by Cristian Espes on 20/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
